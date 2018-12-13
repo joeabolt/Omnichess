71169761ec9438fef1966ce91617caed11cfbecf
SanKeshun <joe@zymurlogic.net>
2018-12-12 20:30:00 -0600

    Helper method for component creation

--- src/Vector.js
+++ src/Vector.js
@@ -23,61 +23,70 @@ class Vector
 			console.error(`Improperly formatted vector: ${str}`);
 		}
 		
+		function buildComponent(compStr, globalStr)
+		{
+			var comp = new Component(0, 1, false, false, false);
+			comp.length = Number(compStr.match(/(-?\d+)/g)[0].slice(1));
+			
+			// Check for jump on first component or end
+			if (compStr.includes("j") || globalStr.includes("j"))
+			{
+				comp.jump = true;
+			}
+			
+			// Check for hop on first component or end
+			if (compStr.includes("h") || globalStr.includes("h"))
+			{
+				comp.hop = true;
+			}
+			
+			// Check for promote on first component or end
+			if (compStr.includes("p") || globalStr.includes("p"))
+			{
+				comp.promote = true;
+			}
+			
+			// Check for finite repetition on end
+			var repetition = globalStr.match(/{(\d+)}/g);
+			if (repetition)
+			{
+				comp.maxRep = Number(repetition[0].slice(2, -1));
+			}
+			
+			// Check for infinite repetition on end
+			if(globalStr.includes("+"))
+			{
+				comp.maxRep = 100; // arbitrarily large
+			}
+	
+			// Check for finite specific repeition
+			repetition = compStr.match(/{(\d+)}/);
+			if (repetition)
+			{
+				comp.maxRep = Number(repetition[1]);
+			}
+			
+			// Check for infinite specific repetition
+			if (compStr.includes("+"))
+			{
+				comp.maxRep = 100; // arbitrarily large
+			}
+			
+			return comp;
+		}
+		
+		// Identify components
+		var endStr = str.slice(str.indexOf(")")+1);
+		var parts = str.slice(str.indexOf("(")+1, str.indexOf(")")).split(",");
+		
 		/* Build x component */
-		var xComp = new Component(0, 1, false, false, false);
-		
-		// Get x length
-		var length = Number(str.match(/\((-?\d+)/g)[0].slice(1));
-		xComp.length = length;
-		
-		// Check for jump on first component or end
-		if (str.match(/(\([^j,]+j[\d{}+hpmd]*, -?\d+[\d{}hpmd]*\))|(\)[\d{}+hpmd]*j)/g))
-		{
-			xComp.jump = true;
-		}
-		
-		// Check for hop on first component or end
-		if (str.match(/(\([^h,]+h[\d{}+jpmd]*, -?\d+[\d{}jpmd]*\))|(\)[\d{}+jpmd]*h)/g))
-		{
-			xComp.hop = true;
-		}
-		
-		// Check for promote on first component or end
-		if (str.match(/(\([^p,]+p[\d{}+jhmd]*, -?\d+[\d{}jhmd]*\))|(\)[\d{}+jhmd]*p)/g))
-		{
-			xComp.promote = true;
-		}
-		
-		// Check for finite repetition on end
-		var repetition = str.match(/\)[jhpmd]*{(\d)}[jhpmd]*/g);
-		if (repetition)
-		{
-			xComp.maxRep = Number(repetition[0].slice(2, -1));
-		}
-		
-		// Check for infinite repetition on end
-		if(str.match(/\)[jhpmd]*\+[jhpmd]*/))
-		{
-			xComp.maxRep = 100; // arbitrarily large
-		}
-
-		// Check for finite specific repeition
-		repetition = str.match(/\(-?\d[jhpmd]*{(\d+)}/);
-		if (repetition)
-		{
-			xComp.maxRep = Number(repetition[1]);
-		}
-		
-		// Check for infinite specific repetition
-		if (str.match(/\(-?\d[jhpmd]*\+/))
-		{
-			xComp.maxRep = 100; // arbitrarily large
-		}
-		
+		var xComp = buildComponent(parts[0], endStr);
 		console.log(xComp);
 		
 		/* Build y component */
+		var yComp = buildComponent(parts[1], endStr);
+		console.log(yComp);
 		
-		return [];
+		return [new Vector(xComp, yComp)];
 	}
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 /* Initial script */
 
-console.log(Vector.create("(1, 0)"));
-console.log(Vector.create("(1+, 0)"));
-console.log(Vector.create("(1, 0)+"));
-console.log(Vector.create("(1{3}, 0)"));
-console.log(Vector.create("(1, 0){3}"));
-console.log(Vector.create("(1d, 0)"));
-console.log(Vector.create("(1, 0d)"));
-console.log(Vector.create("(1p, 0)"));
-console.log(Vector.create("(1, 0)p"));
-console.log(Vector.create("(1h, 0)"));
-console.log(Vector.create("(1, 0)h"));
-console.log(Vector.create("(1j, 0)"));
-console.log(Vector.create("(1, 0)j"))
+console.log(Vector.create("(2p, 1{3})j"));
+// console.log(Vector.create("(1, 0)"));
+// console.log(Vector.create("(1+, 0)"));
+// console.log(Vector.create("(1, 0)+"));
+// console.log(Vector.create("(1{3}, 0)"));
+// console.log(Vector.create("(1, 0){3}"));
+// console.log(Vector.create("(1d, 0)"));
+// console.log(Vector.create("(1, 0d)"));
+// console.log(Vector.create("(1p, 0)"));
+// console.log(Vector.create("(1, 0)p"));
+// console.log(Vector.create("(1h, 0)"));
+// console.log(Vector.create("(1, 0)h"));
+// console.log(Vector.create("(1j, 0)"));
+// console.log(Vector.create("(1, 0)j"));
