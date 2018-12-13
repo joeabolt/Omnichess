3bd62e2978f7043b5d28e70c690e462a803ba31b
SanKeshun <joe@zymurlogic.net>
2018-12-12 19:43:05 -0600

    X component parsing, begin work on testing

--- src/Vector.js
+++ src/Vector.js
@@ -6,14 +6,21 @@ class Vector
 		this.components = [x, y];
 	}
 	
+	/**
+	 *  Creates one or more Vectors based on the string passed in.
+	 *  Returns an array of Vectors containing all Vectors described in the string.
+	 *
+	 *  If an invalidly formatted string is passed in, an error is thrown.
+	 */
 	static create(str)
 	{
+		// TODO: Get all the segments individually
 		str = str.match(/([^;\n]+?)(?=;|$)/gm)[0].trim();
-		var matches = str.match(/(-?\d[\d{}+jhpmd]*, -?\d[\d{}+jhpmd]*\)[\d{}+jhpmd]*/g);
+		var matches = str.match(/(-?\d+[\d{}+jhpmd]*, -?\d+[\d{}+jhpmd]*\)[\d{}+jhpmd]*/g);
 		if (matches <= 0)
 		{
 			// improperly formatted vector notation
-			throw new Error("Improperly formatted vector: " + str);
+			console.error(`Improperly formatted vector: ${str}`);
 		}
 		
 		/* Build x component */
@@ -24,17 +31,53 @@ class Vector
 		xComp.length = length;
 		
 		// Check for jump on first component or end
-		if (str.match(/(\([^j,]+j[\d{}+hpmd]*, -?\d+[\d{}hpmd]*\))|(\)[\d{}+hpmd]*j[\d{}+hpmd]*)/g).length > 0)
+		if (str.match(/(\([^j,]+j[\d{}+hpmd]*, -?\d+[\d{}hpmd]*\))|(\)[\d{}+hpmd]*j)/g).length > 0)
 		{
 			xComp.jump = true;
 		}
 		
 		// Check for hop on first component or end
-		if (str.match(/(\([^h,]+h[\d{}+jpmd]*, -?\d+[\d{}jpmd]*\))|(\)[\d{}+jpmd]*h[\d{}+jpmd]*)/g).length > 0)
+		if (str.match(/(\([^h,]+h[\d{}+jpmd]*, -?\d+[\d{}jpmd]*\))|(\)[\d{}+jpmd]*h)/g).length > 0)
 		{
 			xComp.hop = true;
 		}
 		
+		// Check for promote on first component or end
+		if (str.match(/(\([^p,]+p[\d{}+jhmd]*, -?\d+[\d{}jhmd]*\))|(\)[\d{}+jhmd]*p)/g).length > 0)
+		{
+			xComp.promote = true;
+		}
+		
+		// Check for finite repetition on end
+		var repetition = str.match(/\)[jhpmd]*{(\d)}[jhpmd]*/g);
+		if (repetition.length > 0)
+		{
+			xComp.maxRep = Number(repetition[0]);
+		}
+		
+		// Check for infinite repetition on end
+		if(str.match(/\)[jhpmd]*\+[jhpmd]*/).length > 0)
+		{
+			xComp.maxRep = 100; // arbitrarily large
+		}
+
+		// Check for finite specific repeition
+		repetition = str.match(/\(-?\d[jhpmd]*{(\d+)}/);
+		if (repetition.length > 0)
+		{
+			xComp.maxRep = Number(repetition[0]);
+		}
+		
+		// Check for infinite specific repetition
+		if (str.match(/\(-?\d[jhpmd]*\+/))
+		{
+			xComp.maxRep = 100; // arbitrarily large
+		}
+		
+		console.log(xComp);
+		
 		/* Build y component */
+		
+		return [];
 	}
 }
--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 /* Initial script */
+
+/* load other scripts */
+function loadjs(filename)
+{
+	var script = document.createElement("script");
+	script.setAttribute("type", "text/javascript");
+	script.setAttribute("src", filename);
+}
+
+// Order matters - prerequisites before their dependents
+var scriptsToLoad = ["Component.js", "Vector.js"];
+
+scriptsToLoad.forEach(loadjs);
+
+var alpha = new Vector(1, 2);
+var beta = new Component(1, 1, false, false, false);
