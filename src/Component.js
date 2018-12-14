fa39600e26664d58685ad664888fc7282ad756af
SanKeshun <joe@zymurlogic.net>
2018-12-14 00:23:46 -0600

    Move helper method to Component.Create()

--- src/Component.js
+++ src/Component.js
@@ -9,4 +9,59 @@ class Component
 		this.hop = hop;
 		this.promote = promote;
 	}
+	
+	static Create(compStr, globalStr)
+	{
+		var comp = new Component(0, 1, false, false, false);
+		comp.length = Number(compStr.match(/(-?\d+)/g)[0]);
+		
+		/* Check for jump on first component or end */
+		if (compStr.includes("j") || globalStr.includes("j"))
+		{
+			comp.jump = true;
+		}
+		
+		/* Check for hop on first component or end */
+		if (compStr.includes("h") || globalStr.includes("h"))
+		{
+			comp.hop = true;
+		}
+		
+		/* Check for promote on first component or end */
+		if (compStr.includes("p") || globalStr.includes("p"))
+		{
+			comp.promote = true;
+		}
+		
+		/* Check for finite repetition on end */
+		var repetition = globalStr.match(/{(\d+)}/g);
+		if (repetition)
+		{
+			comp.maxRep = Number(repetition[0].slice(2, -1));
+		}
+		
+		/* Check for infinite repetition */
+		if(compStr.includes("+") || globalStr.includes("+"))
+		{
+			comp.maxRep = 100; /* arbitrarily large */
+		}
+
+		/* Check for finite specific repeition */
+		repetition = compStr.match(/{(\d+)}/);
+		if (repetition)
+		{
+			comp.maxRep = Number(repetition[1]);
+		}
+		
+		/* Check for directional or zero length */
+		if (compStr.includes("d") || globalStr.includes("d") || comp.length == 0)
+		{
+			return [comp];
+		}
+		
+		var reverseComp = new Component(comp.length * -1, comp.maxRep, 
+			comp.jump, comp.hop, comp.promote);
+			
+		return [comp, reverseComp];
+	}
 }
--- src/Vector.js
+++ src/Vector.js
@@ -16,62 +16,6 @@ class Vector
 	 */
 	static Create(str)
 	{
-		/* helper function */
-		function buildComponent(compStr, globalStr)
-		{
-			var comp = new Component(0, 1, false, false, false);
-			comp.length = Number(compStr.match(/(-?\d+)/g)[0]);
-			
-			/* Check for jump on first component or end */
-			if (compStr.includes("j") || globalStr.includes("j"))
-			{
-				comp.jump = true;
-			}
-			
-			/* Check for hop on first component or end */
-			if (compStr.includes("h") || globalStr.includes("h"))
-			{
-				comp.hop = true;
-			}
-			
-			/* Check for promote on first component or end */
-			if (compStr.includes("p") || globalStr.includes("p"))
-			{
-				comp.promote = true;
-			}
-			
-			/* Check for finite repetition on end */
-			var repetition = globalStr.match(/{(\d+)}/g);
-			if (repetition)
-			{
-				comp.maxRep = Number(repetition[0].slice(2, -1));
-			}
-			
-			/* Check for infinite repetition */
-			if(compStr.includes("+") || globalStr.includes("+"))
-			{
-				comp.maxRep = 100; /* arbitrarily large */
-			}
-	
-			/* Check for finite specific repeition */
-			repetition = compStr.match(/{(\d+)}/);
-			if (repetition)
-			{
-				comp.maxRep = Number(repetition[1]);
-			}
-			
-			/* Check for directional or zero length */
-			if (compStr.includes("d") || globalStr.includes("d") || comp.length == 0)
-			{
-				return [comp];
-			}
-			
-			var reverseComp = new Component(comp.length * -1, comp.maxRep, 
-				comp.jump, comp.hop, comp.promote);
-				
-			return [comp, reverseComp];
-		}
-		
 		/* Make all possible vectors */
 		var toReturn = [];
 		var substrings = str.split(";");
@@ -95,10 +39,10 @@ class Vector
 			var parts = vectorString.slice(vectorString.indexOf("(")+1, vectorString.indexOf(")")).split(",");
 			
 			/* Build x component */
-			var xComp = buildComponent(parts[0], endStr);
+			var xComp = Component.Create(parts[0], endStr);
 			
 			/* Build y component */
-			var yComp = buildComponent(parts[1], endStr);
+			var yComp = Component.Create(parts[1], endStr);
 			
 			/* Cross product all x and y components to produce directional vectors */
 			for (var j = 0; j < xComp.length; j++)
