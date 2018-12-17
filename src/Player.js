b609f236236cb6a9731944faf9d72a2ec925745e
SanKeshun <joe@zymurlogic.net>
2018-12-17 16:23:21 -0600

    Create Player class stub

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,15 @@
 /* Represents a player in the game; either computer-controlled or human-controlled */
 class Player 
 {
+	constructor(ownedPieces, capturedPieces)
+	{
+		this.ownedPieces = ownedPieces;
+		this.capturedPieces = capturedPieces;
+	}
 	
+	getMove()
+	{
+		/* Stub */
+		// TODO: Ask the player for a move to make
+	}
 }
