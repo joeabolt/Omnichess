3f22980436dddabd7a531af8952cf995158aec4a
SanKeshun <joe@zymurlogic.net>
2018-12-18 09:21:10 -0600

    Add direction, inPlayPieces, documentation

--- src/Player.js
+++ src/Player.js
@@ -1,8 +1,19 @@
 /* Represents a player in the game; either computer-controlled or human-controlled */
 class Player 
 {
-	constructor(ownedPieces, capturedPieces)
+	/**
+	 *  Constructs a player with the passed paramters. Direction is
+	 *  expected as an array with one entry per dimension, as either
+	 *  1, 0, or -1 indicating the "forward" direction for this player.
+	 *  
+	 *  All others are lists of pieces belonging to this player; inPlay
+	 *  are pieces on the board, owned are pieces not on the board, captured
+	 *  are pieces captured from the enemy.
+	 */
+	constructor(direction, inPlayPieces, ownedPieces, capturedPieces)
 	{
+		this.direction = direction;
+		this.inPlayPieces = inPlayPieces;
 		this.ownedPieces = ownedPieces;
 		this.capturedPieces = capturedPieces;
 	}
