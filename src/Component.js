54425789832899d5321b9dad597b097eb84c59c7
Jake Koperski <wijakoperski@gmail.com>
2019-01-14 14:32:47 -0600

    Cleaned up Component.Create method

--- src/Component.js
+++ src/Component.js
@@ -13,61 +13,44 @@ class Component
 	
 	static Create(componentString, globalString)
 	{
-		const comp = new Component(0, 1, false, false, false);
-		comp.length = Number(componentString.match(/(-?\d+)/g)[0]);
+		const component = new Component(0, 1, false, false, false);
+		component.length = Number(componentString.match(/(-?\d+)/g)[0]);
 		
 		/* Check for jump on first component or end */
-		if (componentString.includes("j") || globalString.includes("j"))
-		{
-			comp.jump = true;
-		}
+		component.jump = componentString.includes("j") || globalString.includes("j");
 		
 		/* Check for hop on first component or end */
-		if (componentString.includes("h") || globalString.includes("h"))
-		{
-			comp.hop = true;
-		}
+		component.hop = componentString.includes("h") || globalString.includes("h");
 		
 		/* Check for promote on first component or end */
-		if (componentString.includes("p") || globalString.includes("p"))
-		{
-			comp.promote = true;
-		}
+		component.promote = componentString.includes("p") || globalString.includes("p");
 		
 		/* Check for finite repetition on end */
-		const finiteEndRepetition = globalString.match(/{(\d+)}/g);
-		if (finiteEndRepetition)
+		const globalFiniteRepetition = globalString.match(/{(\d+)}/g);
+		if (globalFiniteRepetition)
 		{
-			comp.maxRep = Number(finiteEndRepetition[0].slice(2, -1));
+			component.maxRep = Number(globalFiniteRepetition[0].slice(2, -1));
 		}
 		
 		/* Check for infinite repetition */
 		if(componentString.includes("+") || globalString.includes("+"))
 		{
-			comp.maxRep = 100; /* arbitrarily large */
+			component.maxRep = 100; /* arbitrarily large */
 		}
 
 		/* Check for finite specific repeition */
-		const finiteSpecificRepetition = componentString.match(/{(\d+)}/);
-		if (finiteSpecificRepetition)
+		const localFiniteRepetition = componentString.match(/{(\d+)}/);
+		if (localFiniteRepetition)
 		{
-			comp.maxRep = Number(finiteSpecificRepetition[1]);
+			component.maxRep = Number(localFiniteRepetition[1]);
 		}
 		
-		if (comp.length === 0)
+		if (component.length === 0)
 		{
-			comp.maxRep = 1; // slight efficiency, clean output for debugging
+			component.maxRep = 1; // slight efficiency, clean output for debugging
 		}
 		
-		/* Check for directional or zero length */
-		if (componentString.includes("d") || globalString.includes("d") || comp.length == 0)
-		{
-			return [comp];
-		}
-		
-		const reverseComp = new Component(comp.length * -1, comp.maxRep, 
-			comp.jump, comp.hop, comp.promote);
-			
-		return [comp, reverseComp];
+		const reversedComponent = new Component(-component.length, component.maxRep, component.jump, component.hop, component.promote);
+		return componentString.includes("d") || globalString.includes("d") || component.length === 0 ? [component] : [component, reversedComponent];
 	}
 }
