37dec53aeaed31d03f49de580a8786df21f7d5d3
Jake Koperski <wijakoperski@gmail.com>
2019-01-17 12:49:55 -0600

    Remove redundant Vector unit test

--- test/Vector.spec.js
+++ test/Vector.spec.js
@@ -6,15 +6,7 @@ QUnit.test("Vector parses notation with a single element", function (assert) {
     assert.deepEqual(actual, expected);
 });
 
-QUnit.test("Vector parses notation with a single element and a single semicolon", function (assert) {
-    const unit = new Component(1, 1, false, false, false);
-    const inverse = new Component(-1, 1, false, false, false);
-    const expected = [new Vector(unit, unit), new Vector(unit, inverse), new Vector(inverse, unit), new Vector(inverse, inverse)];
-    const actual = Vector.Create("(1, 1);");
-    assert.deepEqual(actual, expected);
-});
-
-QUnit.test("Vector parses notation with a single element and multiple semicolons", function (assert) {
+QUnit.test("Vector parses notation with a single element and ignores semicolons", function (assert) {
     const unit = new Component(1, 1, false, false, false);
     const inverse = new Component(-1, 1, false, false, false);
     const expected = [new Vector(unit, unit), new Vector(unit, inverse), new Vector(inverse, unit), new Vector(inverse, inverse)];
